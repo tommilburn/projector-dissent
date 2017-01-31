e8c2722ca57a95d974687eccca0828e2ba33d80d
Tom Milburn <thomasjosephmilburn@gmail.com>
2017-01-31 15:55:11 -0500

    updates to html page, first functional message passing

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+      output: process.stdout
+});
 
 server.listen(3000);
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + "/public/projector.html");
+  res.sendFile(__dirname + "/public/projector.html");
 });
 
 io.on('connection', function(socket){
-  socket.emit('news', {hello:'world'});
+  setInterval(function(){
+    socket.emit('newtext', {data:'REFUGEES ARE WELCOME HERE'});
+  }, 10000);
+  socket.emit('newtext', {data:'REFUGEES ARE WELCOME HERE'});
 });
+
+function getText(){
+	rl.question('enter the message:\n', function(message){
+		io.emit('newtext', {message: message});
+		getText();
+	})
+}
+getText();
