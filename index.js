2cc546cc3f6c9e8e9b74338f1aa64cd5631f8c56
Tom Milburn <thomasjosephmilburn@gmail.com>
2017-02-02 21:52:31 -0500

    working mobile admin page

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var readline = require('readline');
 
-const rl = readline.createInterface({
-	input: process.stdin,
-      output: process.stdout
-});
+var message = '.';
 
 server.listen(3000);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/public/projector.html");
 });
-
-io.on('connection', function(socket){
-  setInterval(function(){
-    socket.emit('newtext', {data:'REFUGEES ARE WELCOME HERE'});
-  }, 10000);
-  socket.emit('newtext', {data:'REFUGEES ARE WELCOME HERE'});
+app.get('/admin', function(req, res){
+  res.sendFile(__dirname + "/public/admin.html");
 });
 
-function getText(){
-	rl.question('enter the message:\n', function(message){
-		io.emit('newtext', {message: message});
-		getText();
-	})
-}
-getText();
+io.on('connection', function(socket){
+  console.log('connection');
+  socket.emit('newmessage', message);
+  socket.on('setmessage', function(data){
+    message = data;
+    socket.broadcast.emit('newmessage', message);
+  });
+});
+
